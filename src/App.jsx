17273e0571ade6ccef4c992378c7b0bd1d38ebc3
rfaraf <mohsinarfat53@gamil.com>
2024-03-16 01:34:41 +0600

    nav done and some image files imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="bg-red-400">Vite + React</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header></Header>
     </>
   );
 }
